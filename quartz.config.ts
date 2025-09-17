69392527d6eeb24e4ed8994d92b865786ff17cbe
timakimit . <timakimit@gmail.com>
2025-09-17 18:44:23 +0500

    vault backup: 2025-09-17 18:44:23

--- quartz.config.ts
+++ quartz.config.ts
@@ -78,7 +78,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      // Plugin.FolderPage(),
+      Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
